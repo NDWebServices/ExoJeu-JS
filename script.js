c1e34c3c637236abed4566698b35f0f07cd0bc03
NDWebServices <nick.delattre4@gmail.com>
2024-01-03 20:39:57 +0100

    1st commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var perso = document.querySelector(".perso");
+var obstacles = document.querySelector(".obstacles");
+
+function sauter() {
+    if(perso.classList != "animation") {
+        perso.classList.add('animation');
+    }
+    setTimeout(function() {
+        perso.classList.remove('animation');
+    },500)
+}
+
+var verification = setInterval(function() {
+    var persoTop = parseInt(window.getComputedStyle(perso).getPropertyValue("top"));
+    var obstaclesLeft = parseInt(window.getComputedStyle(obstacles).getPropertyValue("left"));
+
+    if(obstaclesLeft<20 &&obstaclesLeft>0 && persoTop>=130) {
+        obstacles.style.animation = "none";
+        alert("Vouas avez perdu comme une grosse merde !")
+    }
+})
